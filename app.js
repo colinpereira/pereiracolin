b0c596d74d7cdf67293f5c64dc7c51392ec99eb3
colinpereira <64594861+colinpereira@users.noreply.github.com>
2021-02-03 23:28:33 -0800

    fixed errors

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
 menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
 });
-
